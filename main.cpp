7bde36065967b5c054e9c6dcb55d224823fc4711
TannerLow <lowthorpt@gmail.com>
2019-10-10 23:32:01 -0700

    Converted to use tokens instead of chars

--- main.cpp
+++ main.cpp
@@ -1,65 +1,100 @@
 #include <iostream>
 #include <stack>
 #include <string>
-int getPrec(char c){
-    switch(c){
-    case '+': return 0;
-    case '-': return 0;
-    case '*': return 1;
-    case '/': return 1;
-    case '^': return 2;
-    default : return -1;
-    }
+#include <vector>
+
+int getPrec(std::string s){
+    if     (s == "+") return 0;
+    else if(s == "-") return 0;
+    else if(s == "*") return 1;
+    else if(s == "/") return 1;
+    else if(s == "^") return 2;
+    else              return -1;
 }
 
-void pushToStack(std::string &v, std::stack<char> &s, char c){
-    if(c == ')'){
-        while(s.top() != '('){
-            char tmp = s.top();
+void pushToStack(std::vector<std::string> &v, std::stack<std::string> &s, std::string token){
+    if(token == ")"){
+        while(s.top() != "("){
+            std::string tmp = s.top();
             s.pop();
             v.push_back(tmp);
         }
         s.pop();
     }
-    else if(c == '(' || !s.empty() && s.top() == '('){
-        s.push(c);
+    else if(token == "(" || !s.empty() && s.top() == "("){
+        s.push(token);
     }
     else{
-        while(!s.empty() && getPrec(s.top()) >= getPrec(c)){
-            char tmp = s.top();
+        while(!s.empty() && getPrec(s.top()) >= getPrec(token)){
+            std::string tmp = s.top();
             s.pop();
             v.push_back(tmp);
         }
-        s.push(c);
+        s.push(token);
     }
 }
 
-void eqToPostfix(std::string input){
-    std::stack<char> operators;
-    std::string postfix;
+void eqToPostfix(std::vector<std::string> input){
+    std::stack<std::string> operators;
+    std::vector<std::string> postfix;
 
     for(int i = 0; i < input.size(); i++){
-        char c = input[i];
-        if(c >= '0' && c <= '9') postfix.push_back(c);
+        char c = input[i][0];
+        if(c >= '0' && c <= '9') postfix.push_back(input[i]);
         else{
-            pushToStack(postfix,operators,c);
+            pushToStack(postfix,operators,input[i]);
         }
     }
     while(!operators.empty()){
-        char tmp = operators.top();
+        std::string tmp = operators.top();
         operators.pop();
         postfix.push_back(tmp);
     }
 
-    std::cout << postfix << std::endl;
+    for(int i = 0; i < postfix.size(); i++)
+        std::cout << postfix[i] << " ";
+    std::cout << std::endl;
+}
+
+void tokenizeHelper(std::string &current, std::vector<std::string> &tokens, char c){
+    if(current.size() > 0){
+        tokens.push_back(current);
+        current = "";
+    }
+    tokens.push_back(std::string(1,c));
+}
+
+std::vector<std::string> tokenize(std::string s){
+    std::vector<std::string> tokens;
+    std::string currentToken = "";
+    for(int i = 0; i < s.size(); i++){
+        char c = s[i];
+        switch(c){
+        case '+': tokenizeHelper(currentToken,tokens,c); break;
+        case '-': tokenizeHelper(currentToken,tokens,c); break;
+        case '*': tokenizeHelper(currentToken,tokens,c); break;
+        case '/': tokenizeHelper(currentToken,tokens,c); break;
+        case '^': tokenizeHelper(currentToken,tokens,c); break;
+        case '(': tokenizeHelper(currentToken,tokens,c); break;
+        case ')': tokenizeHelper(currentToken,tokens,c); break;
+        default : currentToken += c;                     break;
+        }
+    }
+    if(currentToken.size() > 0)
+        tokens.push_back(currentToken);
+    return tokens;
 }
 
 int main(){
     std::string in;
     while(true){
         std::cin >> in;
+        std::vector<std::string> tokens = tokenize(in);
+        /*for(int i = 0; i < tokens.size(); i++){
+            std::cout << tokens[i] << std::endl;
+        }*/
         if(in == "exit") return 0;
-        eqToPostfix(in);
+        eqToPostfix(tokens);
     }
     return 0;
 }
